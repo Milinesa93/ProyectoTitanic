5fd5381087b68bc790a3e2cc31639b8f10cb174e
Milinesa93 <45246940+Milinesa93@users.noreply.github.com>
2024-06-16 22:10:45 +0200

    Limpieza cabin, embarked, age done

--- demo.py
+++ demo.py
@@ -8,6 +8,7 @@ import numpy as np
 import plotly_express as px
 import random
 import plotly.graph_objects as go
+import statistics
 
 from urllib.error import URLError
 
@@ -33,10 +34,10 @@ try:
         
     # Paso 1: Qué columnas tienen valores nulos?
 
-    valores_nulos = df.isnull().sum().reset_index()
-    valores_nulos.columns = ['Columnas', 'Número de Valores Nulos']
-    valores_nulos['Porcentaje nulos'] = ((valores_nulos["Número de Valores Nulos"] / len(df)) * 100).round(2)
-    st.write("Calcular el número de valores nulos por columna", valores_nulos.sort_values(by="Número de Valores Nulos", ascending=False))
+    valores_nulos_original = df.isnull().sum().reset_index()
+    valores_nulos_original.columns = ['Columnas', 'Número de Valores Nulos']
+    valores_nulos_original['Porcentaje nulos'] = ((valores_nulos_original["Número de Valores Nulos"] / len(df)) * 100).round(2)
+    st.write("Calcular el número de valores nulos por columna", valores_nulos_original.sort_values(by="Número de Valores Nulos", ascending=False))
 
     # Paso 2: Cálculo del porcentaje de filas con atributos nulos
     def calcular_porcentaje_filas_nulas(df):
@@ -49,11 +50,24 @@ try:
     porcentaje_nulos = calcular_porcentaje_filas_nulas(df)
     st.write(f"Porcentaje de filas con al menos un valor nulo: {porcentaje_nulos:.2f}%")
 
-    
-    st.write("Cabin 77.10% valores nulos")
-    st.write("Age 19.87% valores nulos")
-    st.write("Embarked 0.22% valores nulos")
-     
+    porcentajes_nulos = {
+    "Cabin": 77.10,
+    "Age": 19.87,
+    "Embarked": 0.22
+    }
+
+# Indicadores de Progreso
+    st.write("### Indicadores de Valores Nulos")
+    for columna, porcentaje in porcentajes_nulos.items():
+        st.write(f"{columna}: {porcentaje}%")
+        st.progress(porcentaje / 100)
+
+     # Tarjetas informativas usando st.metric
+    st.write("### Tarjetas Informativas")
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Cabin", f"{porcentajes_nulos['Cabin']}%", delta=None)
+    col2.metric("Age", f"{porcentajes_nulos['Age']}%", delta=None)
+    col3.metric("Embarked", f"{porcentajes_nulos['Embarked']}%", delta=None)
 
 # Calcular el número de valores nulos por columna
     valores_nulos = df.isnull().sum().reset_index()
@@ -140,8 +154,52 @@ try:
     st.plotly_chart(fig_filtered)
     
     st.write("#### Tratamiento de valores nulos col Embarked, con la moda")
-    st.write("###### ¿Por qué con la moda?: Al no ser un dato númerico, se busca la categoria que más se repite")
+    st.write("###### ¿Por qué con la moda?: Al no ser un dato númerico, se busca la categoria que más se repite.")
+    
+    # Calcular la moda de la columna "Embarked"
+    moda_embarked_corregido = df['Embarked'].mode()[0]
+    
+    df["Embarked"].fillna(moda_embarked_corregido, inplace=True)
+    nulos_embarked = df["Embarked"].isnull().sum()
+    
+    st.write("#### Tratamiento de valores nulos col Cabin")
+    st.write("###### ")
+    
+    # Tratamiento de la columna Cabin: crear otra para indicar que hay info o no.
 
+    # Crear una nueva columna indicadora de la presencia de datos en 'Cabin'
+        # Update the 'Cabin' column to replace NaN values with 'Unknown'
+    df['Cabin'] = df['Cabin'].fillna('Unknown')
+    
+    # Create a new indicator column for the presence of data in 'Cabin'
+    df['Cabin_Ind'] = df['Cabin'] != 'Unknown'
+    
+    # Verify there are no null values in the 'Cabin' column
+    null_values_after = df['Cabin'].isnull().sum()
+    
+    st.write(df[['Cabin_Ind']])
+   
+
+# Recalcular los valores nulos después del tratamiento
+    valores_nulos_actualizados = df.isnull().sum().reset_index()
+    valores_nulos_actualizados.columns = ['Columnas', 'Número de Valores Nulos']
+    valores_nulos_actualizados['Porcentaje nulos'] = ((valores_nulos_actualizados["Número de Valores Nulos"] / len(df)) * 100).round(2)
+
+    # Recalcular el porcentaje de filas con atributos nulos después del tratamiento
+    porcentaje_nulos_actualizados = calcular_porcentaje_filas_nulas(df)
+
+# Porcentajes de valores nulos en las columnas específicas después del tratamiento
+    porcentajes_nulos_actualizados = {
+    "Cabin": valores_nulos_actualizados[valores_nulos_actualizados['Columnas'] == 'Cabin_Ind']['Porcentaje nulos'].values[0],
+    "Age": valores_nulos_actualizados[valores_nulos_actualizados['Columnas'] == 'Age']['Porcentaje nulos'].values[0],
+    "Embarked": valores_nulos_actualizados[valores_nulos_actualizados['Columnas'] == 'Embarked']['Porcentaje nulos'].values[0]
+}
+    st.subheader('Porcentaje de valores nulos después del tratamiento')
+    col1, col2, col3 = st.columns(3)
+    
+    col1.metric(label="Cabin", value=f"{porcentajes_nulos_actualizados['Cabin']:.2f}%")
+    col2.metric(label="Age", value=f"{porcentajes_nulos_actualizados['Age']:.2f}%")
+    col3.metric(label="Embarked", value=f"{porcentajes_nulos_actualizados['Embarked']:.2f}%")
 
 except URLError as e:
      st.error(
